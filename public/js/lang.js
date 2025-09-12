66b77e065f674dd984ffa1ad6daa4e33d54823ed
Asdasd7117 <alywm100@gmail.com>
2025-09-12 18:34:12 +0300

    Update lang.js

--- public/js/lang.js
+++ public/js/lang.js
@@ -1,1 +1,8 @@
-// محتوى lang.js الكامل هنا
+function setLanguage(lang){
+  localStorage.setItem('lang',lang);
+}
+function toggleLanguage(){
+  const lang = localStorage.getItem('lang') || 'ar';
+  setLanguage(lang==='ar'?'en':'ar');
+  alert('تم تبديل اللغة إلى '+(lang==='ar'?'English':'العربية'));
+}
