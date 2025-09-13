217f8be7de72d835b8d8a1e43356d000ea0acbeb
Asdasd7117 <alywm100@gmail.com>
2025-09-13 17:25:23 +0300

    Delete public directory

--- public/js/map.js
+++
@@ -1,8 +0,0 @@
-let map;
-function initMap(){
-  const mapDiv=document.getElementById('map');
-  if(!mapDiv) return;
-  map=new google.maps.Map(mapDiv,{zoom:12,center:{lat:24.7136,lng:46.6753}});
-  // مثال: إضافة marker
-  const marker=new google.maps.Marker({position:{lat:24.7136,lng:46.6753},map:map,title:"موقع المندوب"});
-}
